2b9ca6e2eff8d79c906e96c06228b96acfc24455
Muin Chowdhury <muin.bd@gmail.com>
2025-03-13 05:34:00 +0600

    Javascript diye goniter gangster done

---
+++ Variable/javascript-diye-goniter-gangster.js
@@ -0,0 +1,82 @@
+// Practice: 1
+const income = 100;
+const expense = 40;
+
+const saving = income - expense;
+console.log(saving); // 60
+
+// Practice: 2
+const totalPencil = 10;
+const pencilUse = 2;
+
+const totalPencilUse = totalPencil / pencilUse;
+console.log(totalPencilUse); // 5
+
+// Practice: 3
+const totalMoney = 60;
+const perCost = 15;
+
+const costCount = totalMoney / perCost;
+console.log(costCount); // 4
+
+const restMoney = totalMoney % perCost;
+console.log(restMoney); // 0
+
+// Practice: 4
+const string1 = "Hello";
+const string2 = "World";
+
+const string3 = string1 + string2;
+console.log(string3); // HelloWorld
+
+// Practice: 5
+const chalKg = 153;
+const dalKg = 261;
+
+totalKg = chalKg + dalKg;
+console.log(totalKg); // 414
+
+// Practice: 6
+const totalMoney = 500;
+const perCost = 75;
+
+const costCount = totalMoney / perCost;
+console.log(costCount); // 6
+
+const restMoney = totalMoney % perCost;
+console.log(restMoney); // 50
+
+// Practice: 7
+const num1 = 8;
+const num2 = 3;
+
+const divi = num1 / num2;
+console.log(divi); // 2.6666666666666665
+
+const mod = num1 % num2;
+console.log(mod); // 2
+
+// Practice: 8
+const num1 = 50;
+const num2 = 9;
+
+const mod = num1 % num2;
+console.log(mod); // 5
+
+// Practice: 9
+const string1 = "Bangla";
+const string2 = "desh";
+
+const string3 = string1 + string2;
+console.log(string3); // Bangladesh
+
+// Practice: 10
+const totalRice = 480;
+const perDayRice = 4;
+
+const totalDay = totalRice / perDayRice;
+console.log(totalDay); // 120
+
+const totalMonth = totalDay / 30;
+console.log(totalMonth); // 4
+
