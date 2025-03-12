75427503de2c4a3513fab715f5ff16b160c64304
Muin Chowdhury <muin.bd@gmail.com>
2025-03-12 20:30:10 +0600

    variable ki practices added

---
+++ Variable/variable-ki.js
@@ -0,0 +1,16 @@
+//Variable Ki Practices
+
+const myBirthYear = 1980;
+
+const botherSister = 3;
+
+const students = 100;
+
+const marks = 90;
+
+const rooms = 10;
+
+const studyHours = 5;
+
+const total = 38;
+
