c1f7a30fa66e2ab3ace76d6cbf62abe3aeb38012
Muin Chowdhury <muin.bd@gmail.com>
2025-03-12 20:39:24 +0600

    Log-er goal, console practices added

---
+++ Variable/log-er-goal-console.js
@@ -0,0 +1,23 @@
+
+const currentAge = 44;
+console.log(currentAge); // 44
+
+const booksToRoead = "10";
+console.log(booksToRoead); // 10
+
+const wakeUpTime = 6;
+const wakeUpTime = 7;
+console.log(wakeUpTime); // 7
+
+const wastedTime = 2;
+console.log(wastedTime); // 2
+
+const borrow = 30000;
+console.log(borrow); // 30000
+
+const goals = 3;
+console.log(goals); // 3
+
+const pillow = 3;
+console.log(pillow); // 3
+
