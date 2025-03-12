438f423ee559e3e16542c1c358137311aab5406c
Muin Chowdhury <muin.bd@gmail.com>
2025-03-12 22:00:24 +0600

    Kanamachi true, kanamachi false practice done

---
+++ Variable/kanamachi-true-kanamachi-false.js
@@ -0,0 +1,24 @@
+const isHappy = false;
+console.log(isHappy); // false
+
+const crushMessage = false;
+console.log(crushMessage); // false 
+
+const isCelebrating = true;
+console.log(isCelebrating); // true
+
+const isHappy = true;
+const hasMoney = false;
+const isHungry = true;
+console.log(isHappy); // true
+console.log(hasMoney); // false
+console.log(isHungry); // true
+
+const isStudyDone = false;
+console.log(isStudyDone); // false
+
+const isPhoneCharged = true;
+console.log(isPhoneCharged); // true
+
+const isDinnerDone = true;
+console.log(isDinnerDone); // true
