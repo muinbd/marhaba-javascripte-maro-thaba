e04585e54a4e04c6866b2470271cfd9f67d179b0
Muin Chowdhury <muin.bd@gmail.com>
2025-03-13 19:39:24 +0600

    Let diye shuru, bodlabe guru done

---
+++ Data Type/let-diye-shuru-bodlabe-guru.js
@@ -0,0 +1,37 @@
+//Practice 1
+let age = 44;
+console.log(age); // 44
+
+age = 55;
+console.log(age); // 55
+
+//Practice 2
+let pocketMoney = 500;
+pocketMoney = 150;
+console.log(pocketMoney); // 150
+
+//Practice 3
+let studytime = 4;
+studytime = 0;
+console.log(studytime); // 0
+
+//Practice 4
+let friendsCount = 3;
+friendsCount = friendsCount - 2;
+console.log(friendsCount); // 1
+
+//Practice 5
+let phonePrice = 25000;
+phonePrice = 12000;
+console.log(phonePrice); // 12000
+
+//Practice 6
+let chairsCount = 40;
+chairsCount = chairsCount - 2; 
+chairsCount = chairsCount - 6;
+console.log(chairsCount); // 32
+
+//Practice 7
+let playtime = 3;
+playtime = 0;
+console.log(playtime); // 0
