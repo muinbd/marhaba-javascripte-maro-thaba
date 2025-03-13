5f7ff453094966e8a07272bbe94b18774e66f19f
Muin Chowdhury <muin.bd@gmail.com>
2025-03-13 21:02:17 +0600

    Parse er bhitor nanroti done

---
+++ Data Type/parse-er-bhitor-nanroti.js
@@ -0,0 +1,46 @@
+// Practoce 1
+let num1 = '20';
+let sum = parseInt(num1) + 10;
+console.log(sum); // 30
+
+// Practice 2
+const num2 = '3.14159';
+const num3 = parseInt(num2);
+console.log(num3); // 3
+
+
+// Practice 3
+const num4 = 'premikBoy';
+const num5 = parseInt(num4);
+console.log(num5); // NaN
+
+
+// Practice 4
+const num6 = 3.456;
+const num7 = 2.789;
+const sum2 = num6 + num7;
+const totalNumber = parseFloat(sum2).toFixed(2);
+console.log(totalNumber); // 6.25
+
+
+// Practice 5
+const num8 = '56.78';
+const num9 = '12.34';
+const sum3 = parseFloat(num8) + parseFloat(num9);
+console.log(sum3); // 69.12
+
+
+// Practice 6
+const num10 = '10.5678';
+const num11 = '5.4321';
+const sum4 = parseFloat(num10) + parseFloat(num11);
+const totalNumber2 = sum4.toFixed(1); // 16.0
+
+
+// Practice 7
+const num12 = '42.45689754';
+const num13 = parseFloat(num12).toFixed(3);
+console.log(num13); // 42.457
+
+
+
