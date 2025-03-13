17558c61c75943b2214c8c08f0f4754b0a1ee560
Muin Chowdhury <muin.bd@gmail.com>
2025-03-13 20:24:15 +0600

    O meye tor nam debo ki

---
+++ Data Type/o-meye-tor-nam-debo-ki.js
@@ -0,0 +1,25 @@
+const myFavoriteFood = "Biriyani";
+
+let city = "Chittagong";
+let City = "Dhaka";
+console.log(city); // Chittagong
+console.log(City); // Dhaka 
+
+let userName = "Johan";
+let username = "Jovan";
+console.log(userName); // Johan
+console.log(username); // Jovan
+
+const myHomeAdress = "Kishoreganj";
+console.log(myHomeAdress); // Kishoreganj
+
+const super = 10;
+console.log(super); // SyntaxError: Unexpected strict mode reserved word
+
+
+let class = "Ten";
+console.log(class); // SyntaxError: Unexpected strict mode reserved word
+
+//Need to change variable name, because it's a number and it's not allowed to start with a number
+let 2ndPosition = "Regan";
+console.log(2ndPosition); // SyntaxError: Unexpected number
