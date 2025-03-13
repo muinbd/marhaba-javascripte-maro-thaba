8f208c35db76de1509cbdd2bf25bfa77f2200efc
Muin Chowdhury <muin.bd@gmail.com>
2025-03-13 19:28:15 +0600

    Data Type - type dekhe hype done

---
+++ Data Type/type-dekhe-hype.js
@@ -0,0 +1,18 @@
+const price = 555;
+console.log(typeof price); // number
+
+const learning = true;
+console.log(typeof learning); // boolean
+
+const megaCity = "Dhaka";
+console.log(typeof megaCity); // string
+
+const temperature = 102.5;  
+console.log(typeof temperature); // number
+
+const isHungry = true;
+console.log(typeof isHungry); // boolean
+
+const isReach = true;
+console.log(typeof isReach); // boolean
+
