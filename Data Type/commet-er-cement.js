449e87b75a9841942d9dc7aef18606b343426d07
Muin Chowdhury <muin.bd@gmail.com>
2025-03-13 19:50:52 +0600

    comment-er-cement done

---
+++ Data Type/commet-er-cement.js
@@ -0,0 +1,30 @@
+//Football score
+const score;
+
+//Circle Radius
+//Area of a circle
+//Circumference of a circle
+const radius;
+
+/* My favorite dress */
+const dress;
+
+//Eid Gifted Money
+const giftMoney = 100;
+
+/*
+PHP 1 Hour
+JavaScript 1 Hour
+Premiere Pro 2 Hours
+ShellScripting 1 Hour
+DevOps 2 Hour
+*/
+const studytime = 7;
+
+//Money will receieved by friend 98
+//Service Charge 2 Taka
+const sendMoney = 100;
+
+
+//Product price and delievry charge included
+const pay = 1000;
