a5922f352130656ea27d735e156c1c02fc6313ae
Muin Chowdhury <muin.bd@gmail.com>
2025-03-14 23:16:16 +0600

    Variabler tuluna kokhono bhulu na done

---
+++ Comparison/variabler-tuluna-khono-bhulu-na.js
@@ -0,0 +1,40 @@
+// Practice 1
+const youngerBortherAge = 10;
+const myAge = 15;
+console.log(youngerBortherAge <= myAge); // true
+
+// Practice 2
+const passMarks = 33;
+const myMarks = 45;
+console.log(myMarks >= passMarks); // true
+
+// Practice 3
+const jumboraCost = 100;
+const kodbelCost = 50;
+console.log(jumboraCost < kodbelCost); // false
+
+// Practice 4
+const myReadingBook = 10;
+const myFriendReadingBook = 10;
+console.log(myReadingBook == myFriendReadingBook); // true
+
+// Practice 5
+const myBiscuits = 15;
+const myFriendBiscuits = 20;
+console.log(myBiscuits < myFriendBiscuits); // true
+
+// Practice 6
+const realGoals = 2;
+const barcaGoals = 3;
+console.log(realGoals < barcaGoals); // true
+
+// Practice 7
+const myName = "Rahim";
+const myFriendName = "Karim";
+console.log(myName == myFriendName); // false
+
+// Practice 8
+const isStudy = true;
+const isFriendStudy = false;
+console.log(isStudy == isFriendStudy); // false
+
