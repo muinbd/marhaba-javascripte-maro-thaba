3cfcf03abd55ac2020fff12482eb31ff60798902
Muin Chowdhury <muin.bd@gmail.com>
2025-03-14 22:53:02 +0600

    Pasher basar auntyer tuluna done

---
+++ Comparison/pasher-basar-auntyer-tuluna.js
@@ -0,0 +1,42 @@
+// Practice 1
+const myselfEating = 7;
+const myBrotherEating = 5;
+const whoGetsMore = myselfEating > myBrotherEating; 
+console.log(whoGetsMore); // true
+
+// Practice 2
+const myClassStudents = 45;
+const nextClassStudents = 50;
+const isMyClassBigger = myClassStudents > nextClassStudents;
+console.log(isMyClassBigger); // false
+
+// Practice 3
+const myMarks = 75;
+const myFriendMarks = 75;
+const areMarksEqual = myMarks === myFriendMarks;
+console.log(areMarksEqual); // true
+
+// Practice 4
+const savingTarget = 100;
+const currentSaving = 95;
+const isSavingTargetMet = currentSaving >= savingTarget;
+console.log(isSavingTargetMet); // false
+
+// Practice 5
+const num1 = 10;
+const num2 = 7;
+//const isNumDifferent = num1 != num2;    
+console.log(isNumDifferent); // true
+
+// Practice 6
+const num3 = 20;
+const num4 = 15;
+const isNum1LessEqual = num3 <= num4;
+console.log(isNum1LessEqual); // false
+
+// Practice 7
+const myObtainMarks = 45;
+const myFriendObtainMarks = 97;
+const myFriendObtainMarksLess = myObtainMarks > myFriendObtainMarks;
+console.log(myFriendObtainMarksLess); // true
+
