8959ac8e891c201419dd75e3667f4dd19a273d31
Muin Chowdhury <muin.bd@gmail.com>
2025-03-15 21:24:06 +0600

    Chatro jokhon patro done

---
+++ 04. If-Else/chatro-jhokon-patro.js
@@ -0,0 +1,83 @@
+// Practice 1
+const age = 18;
+const height = 60;
+if (age > 18 && height > 60) {
+    console.log("Gari thelbe"); // true
+} else {
+    console.log("Garite bose thakbe");
+}
+
+// Practice 2
+const mathScore = 80;
+const englishScore = 85;
+if (mathScore > 80 || englishScore > 85) {
+    console.log("Bhorti hoyar joggo"); // true
+} else {
+    console.log("Toke biye diye debo");
+}
+
+// Practice 3
+const studentGPA = 5;
+const familyIncome = 10000;
+if (studentGPA == 5 && familyIncome < 10000) {
+    console.log("Scholarship pabe"); // true
+} else {
+    console.log("Beton diye porte hobe");
+}
+
+// Practice 4
+const employeAge = 30;
+const employeExperience = 2;
+if (employeAge < 30 && employeExperience > 2) {
+    console.log("Porikkha dite parbe"); // true
+} else {
+    console.log("Chakrir porikkha dite parbe na");
+}   
+
+// Practice 5
+const eggCount = 12;
+const isChicken = false;
+if (eggCount > 12 || isChicken == false) {
+    console.log("Dimer korma ranna korbi"); // true
+} else {
+    console.log("Pawruti ar kola khabi");
+}
+
+// Practice 6
+const humanTemperature = 100;
+const isCoughing = true;
+if (humanTemperature > 100 || isCoughing == true) {
+    console.log("Doctor er kache jabe"); // true
+} else {
+    console.log("Kantha muri diye shuye thakbe");
+}
+
+// Practice 7
+const studentPresence = 75;
+const isHomeworkDone = true;
+if (studentPresence > 80 && isHomeworkDone == true) {
+    console.log("Exam dite dewa hobe"); // true
+} else {
+    console.log("Auto fail");
+}
+
+// Practice 8
+const isElectricity = true;
+const isMobileCharged = false;
+if (isElectricity == false && isMobileCharged == false) {
+    console.log("Porte bosbi"); // true
+}
+else {
+    console.log("Video game khelbi");
+}
+
+
+// Practice 9
+const shirtPrice = 1000;
+const isCoupon = true;
+if (shirtPrice > 1000 && isCoupon == true) {
+    console.log("Discount deya hobe"); // true
+}
+else {
+    console.log("No discount");
+}
