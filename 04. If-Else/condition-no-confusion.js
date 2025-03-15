9ce49791669ce11733e42af9e14d74f4a9ca0bcb
Muin Chowdhury <muin.bd@gmail.com>
2025-03-15 19:18:21 +0600

    Condition no confusion done

---
+++ 04. If-Else/condition-no-confusion.js
@@ -0,0 +1,47 @@
+// Practice 1
+const returnHome = 6;
+if (returnHome == 6) {
+    console.log("Nasta khawabe"); // true
+} else {
+    console.log("Jharur bari dibe");
+}
+
+// Practice 2
+const isLogin = true;
+if (isLogin == true) {
+    console.log("Welcome!"); // true
+} else {
+    console.log("Get Lost!");
+}
+
+// Practice 3
+const runningDuration = 5;
+if (runningDuration == 5) {
+    console.log("Chocolete khete dibe"); // true
+} else {
+    console.log("Bhuri mota hobe");
+}
+
+// Practice 4
+const myMarks = 85;
+if (myMarks > 80) {
+    console.log("Bike kine debe"); // true
+} else {
+    console.log("Bike kinte parbe na");
+}
+
+// Practice 5
+const movieShowTime = 9;
+if (movieShowTime == 9) {
+    console.log("Movie dekhabo"); // true
+} else {
+    console.log("Nake tel diye ghumabo");
+}
+
+// Practice 6
+const temperature = 30;
+if (temperature >= 30) {
+    console.log("Aircondition chalabo"); // true
+} else {
+    console.log("Kombol muri diye ghumabo");
+}
